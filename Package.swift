c832aceff5b21c0a90ad5f1d6537d5689840be8b
YANG GUANG <holaux@gmail.com>
2026-02-03 17:20:28 +0800

    Bump minimum deployment target to iOS 15.0 and tvOS 15.0
    This fixes compatibility with Xcode 17 which no longer includes
    swiftCompatibility56 and swiftCompatibilityConcurrency libraries
    that were required for iOS 12 support.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LookinServer",
     platforms: [
-        .iOS(.v12),.tvOS(.v12)
+        .iOS(.v15), .tvOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
